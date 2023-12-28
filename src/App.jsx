d6cec65315136d94ee2f1ad095d533d7d918a7c7
ivanizidoro <ivanizidoro@hotmail.com>
2023-12-28 19:39:36 -0300

    alteração nos filmes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 import { Video } from "./components/video";
 import { Background } from "./components/background";
@@ -7,19 +7,43 @@ import { Header } from "./components/header";
 import { Movies } from "./components/movies";
 
 import { apiData } from './api/data';
+import axios from 'axios';
+
+const DEFAULT_MOVIE = {
+  title: "A Mulher Rei",
+  description:
+    "Em 1800, o general Nanisca treina um grupo de mulheres guerreiras para proteger o reino africano de Dahomey de um inimigo estrangeiro.",
+  imageSmall:
+    "https://vertentesdocinema.com/wp-content/uploads/2022/09/amr-cartazteaser-online-1400x2100px-data.jpg",
+  imageBanner: "https://images8.alphacoders.com/125/1253813.jpg",
+  linkVideo: "u1HTd_VVICQ",
+  category: "movies",
+}
 
 function App() {
-  const [video, setVideo] = useState(apiData?.movies[0])
+  const [video, setVideo] = useState(DEFAULT_MOVIE)
+  const [moviesData, setMoviesData] = useState([])
   const [playVideo, setPlayVideo] = useState(false)
+  const baseUrl = 'http://localhost:3003'
 
   const { title, description, linkVideo, imageBanner } = video
+
+  const apiDataMovies = moviesData?.movies?.length > 0 ? moviesData : apiData
+
+  useEffect(() => {
+    const getMoviesAndCategories = async () => {
+      const { data } = await axios.get(`${baseUrl}/movies-categories`);
+      setMoviesData(data)
+    }
+    getMoviesAndCategories();
+  }, []);
 
   const handleOpenPlayVideo = () => setPlayVideo(true)
   const handleClosePlayVideo = () => setPlayVideo(false)
 
-  const handleVideo = (data) =>{
+  const handleVideo = (data) => {
     setVideo(data)
-    window.scrollTo({top:0});
+    window.scrollTo({ top: 0 });
   };
 
   return <Background
@@ -30,7 +54,7 @@ function App() {
       description={description}
       handleOpenPlayVideo={handleOpenPlayVideo} />
     <Movies
-      data={apiData}
+      data={apiDataMovies}
       handleVideo={(data) => handleVideo(data)}
     />
     <Video playVideo={playVideo}
@@ -40,4 +64,4 @@ function App() {
   </Background>
 }
 
-export default App
+export default App;
